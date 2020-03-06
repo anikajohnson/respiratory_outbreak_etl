053bc9727af04d9f7ee85b75eff9f4105e14d336
Anika Johnson <anikaleejohnson@gmail.com>
2020-03-05 19:04:58 -0600

    added initial csv dataframe load

---
+++ respiratory_outbreaks_db.sql
@@ -0,0 +1,39 @@
+-- Database: respiratory_outbreak_db
+
+-- DROP DATABASE respiratory_outbreak_db;
+
+CREATE DATABASE respiratory_outbreak_db
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+CREATE TABLE sars (
+id INT PRIMARY KEY,
+date TEXT,
+province_state TEXT,
+country TEXT,
+confirmed INT,
+deaths INT,
+recovered INT
+);
+
+CREATE TABLE corona (
+id INT PRIMARY KEY,
+date TEXT,
+province_state TEXT,
+country TEXT,
+confirmed INT,
+deaths INT,
+recovered INT
+);
+
+
+-- Joins tables
+SELECT sars.* 
+FROM sars
+INNER JOIN corona
+ON sars.country = corona.country;
